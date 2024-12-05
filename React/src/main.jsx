69dbde9c7b221a5d38d5ea9a74b8dc2cc2c85c97
Muskaansharma9 <mshar269@my.centennialcollege.ca>
2024-12-05 17:30:01 -0500

    Update main.jsx

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,8 +1,10 @@
+/* Importing necessary modules from React library*/
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' // Importing the main App component*/
 
+/* Creating the root element and rendering the app*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
